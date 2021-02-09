4e1011af95fd6f9af95699a0c605d7a19f3abad9
sedm1 <novikovn383@gmail.com>
2021-02-09 22:36:21 +0500

    new_project
    Спецкранмонтаж

--- user.php
+++
@@ -1,8 +0,0 @@
-<?
-if($_POST['name'] == 'holod'){
-    setcookie('user_holod',1);
-    header('location: /');
-}else{
-    header('Content-Type: text/html; charset=utf-8');
-    echo 'Неверный логин';
-}
