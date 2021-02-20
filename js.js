81105c51101320e0e7a3b9aa53157300daaae49c
sedm1 <novikovn383@gmail.com>
2021-02-20 19:45:15 +0500

    pr

--- js.js
+++ js.js
@@ -1,3 +1,23 @@
 $('.slider').slick({
-  dots: true,
+  infinite: true,
+  speed: 700,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 950,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint : 450,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
 });
