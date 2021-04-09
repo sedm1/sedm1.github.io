113932854ea6d9b3124accfc13247fd11e8f0dd1
sedm1 <novikovn383@gmail.com>
2021-04-09 15:05:21 +0500

    version_3

---
+++ directolog_3/main.js
@@ -0,0 +1,11 @@
+function openMenu(){
+  var menu = document.getElementById('menu2');
+  menu.classList.toggle('block')
+}
+var wind = document.getElementById("window");
+function openWindow(){
+  wind.classList.toggle('flex');
+}
+function closeWindow(){
+  wind.classList.toggle('flex');
+}
