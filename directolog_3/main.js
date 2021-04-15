29640a861fbfe50e0612451262f6d32238b620fa
sedm1 <novikovn383@gmail.com>
2021-04-15 16:57:41 +0500

    js

--- directolog_3/main.js
+++ directolog_3/main.js
@@ -1,15 +1,21 @@
+var menu = document.getElementById('menu2');
+var bg =document.getElementById("black__bg");
+var body = document.getElementById("body");
+var wind = document.getElementById("window");
 function openMenu(){
-  var menu = document.getElementById('menu2');
-  var bg =document.getElementById("black__bg");
-  var body = document.getElementById("body");
   body.classList.toggle("overflow");
   bg.classList.toggle('block');
   menu.classList.toggle('block')
 }
-var wind = document.getElementById("window");
 function openWindow(){
   wind.classList.toggle('flex');
 }
 function closeWindow(){
   wind.classList.toggle('flex');
 }
+addEventListener("click", function(e) {
+    if (e.target === bg ) {
+      menu.classList.toggle('block');
+      bg.classList.toggle('block');
+    };
+  });
