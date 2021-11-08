76cd19e63d9da3545029f93da32e8189601a9761
Николай <novikovn383@gmail.com>
2021-11-08 20:42:35 +0500

    img

--- Reedweeks/js/anim.js
+++ Reedweeks/js/anim.js
@@ -1,28 +1,9 @@
 new Skroll({
 })
-    .add(".info__img",{
-        delay:10,
-        duration:900,
-        animation:"zoomIn"
-    })
-    .add(".info__img_2",{
-        delay:10,
-        duration:900,
-        animation:"zoomIn"
-    })
-    .add(".info__text",{
-        animation:"flipInX",
-        delay:150,
-        duration:600
-    })
-    .add(".categori__button",{
-        delay:10,
-        duration:900,
-        animation:"zoomIn"
-    })
+    
     .addAnimation("topToBottom",{
         start:function(e){
-            e.style["transform"] = "translate(0px, -500px)";
+            e.style["transform"] = "translate(0px, -100px)";
             e.style["opacity"] = 0;
         },
         end:function(e){
@@ -37,8 +18,28 @@ new Skroll({
     })
     .add(".tovar__item",{
         animation:"topToBottom",
-        duration:700,
+        duration:1000,
         delay:100
+    })
+    .add(".info__img",{
+        delay:10,
+        duration:900,
+        animation:"topToBottom",
+    })
+    .add(".info__img_2",{
+        delay:10,
+        duration:900,
+        animation:"topToBottom",
+    })
+    .add(".info__text",{
+        animation:"topToBottom",
+        delay:150,
+        duration:600
+    })
+    .add(".categori__button",{
+        delay:10,
+        duration:900,
+        animation:"zoomIn"
     })
     
     
