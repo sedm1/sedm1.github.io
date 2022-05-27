2f39d92e7bc671f30f95c95972c0f766486d8285
Николай <novikovn383@gmail.com>
2022-05-28 01:06:51 +0500

    ver

--- titan/js/main.js
+++ titan/js/main.js
@@ -9,4 +9,32 @@ $(function() {
         slidesToShow: 10,
         slidesToScroll: 1
     })
+    $(".header__input-item").click(function(){
+        OpenSearch()
+    })
+    $(".clodesearch").click(function(){
+        CloseSearch()
+    })
+    $(".logIn").click(function(){
+        openForm()
+    })
 });
+function OpenSearch(item){
+    $(".logo").hide()
+    $(".mainheader__links").hide()
+    $(".mainheader").addClass("mainheader-active")
+    $(".mainheader__input").addClass("header__input-item-active")
+    $(".search").addClass("search-active")
+    $(".clodesearch").addClass("clodesearch-active")
+}
+function CloseSearch(){
+    $(".logo").show()
+    $(".mainheader__links").show()
+    $(".mainheader").removeClass("mainheader-active")
+    $(".mainheader__input").removeClass("header__input-item-active")
+    $(".search").removeClass("search-active")
+    $(".clodesearch").removeClass("clodesearch-active")
+}
+function openForm(){
+    $(".login__form").toggleClass("login__form-active")
+}
