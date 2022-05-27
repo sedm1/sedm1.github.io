f6a2d96aa0893e3ea3366efdf9f351f275684c8b
Николай <novikovn383@gmail.com>
2022-05-27 22:15:55 +0500

    new_site

---
+++ titan/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $(".main__slider").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    })
+    $(".brands__slider").slick({
+        infinite: true,
+        slidesToShow: 10,
+        slidesToScroll: 1
+    })
+});
