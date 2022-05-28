7aafd3eff7a77f0684d29b2ab9648b3f18bf38fa
Николай <novikovn383@gmail.com>
2022-05-28 21:45:22 +0500

    new_ver

--- titan/js/main.js
+++ titan/js/main.js
@@ -2,14 +2,80 @@ $(function() {
     $(".main__slider").slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 800
     })
     $(".brands__slider").slick({
         infinite: true,
         slidesToShow: 10,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1441,
+                settings: {
+                  slidesToShow: 7,
+                  slidesToScroll: 1,
+                }
+              },
+            {
+              breakpoint: 1260,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 4,
+                  slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+            },
+            
+        ]
     })
-    $(".header__input-item").click(function(){
+    $(".popular__slider").slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        centerMode: true,
+        responsive: [
+            {
+              breakpoint: 1260,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+
+                }
+            }
+            
+          ]
+    })
+    $("#no_mobile-input").click(function(){
         OpenSearch()
     })
     $(".clodesearch").click(function(){
@@ -18,8 +84,23 @@ $(function() {
     $(".logIn").click(function(){
         openForm()
     })
+    var width = $(window).width();
+    if(width <= 1000){
+        $(".header__input-item").attr("placeholder", "Search...");
+    }
+    if(width <=750){
+        var item = $(".categories__item")
+        item.slice(0, 4).css('display', 'flex');
+        $("#loadMore").on("click", function(e){
+            e.preventDefault();
+            $(".categories__item:hidden").slice(0, 4).slideDown().css('display', 'flex');
+            if($(".categories__item:hidden").length == 0) {
+                $("#loadMore").text("No More").addClass("noContent");
+            }
+        });
+    }
 });
-function OpenSearch(item){
+function OpenSearch(){
     $(".logo").hide()
     $(".mainheader__links").hide()
     $(".mainheader").addClass("mainheader-active")
@@ -38,3 +119,4 @@ function CloseSearch(){
 function openForm(){
     $(".login__form").toggleClass("login__form-active")
 }
+
