8592ddf2dbc10920020c8bb6eaf444fa41431509
Николай <novikovn383@gmail.com>
2022-06-11 20:40:10 +0500

    car

--- viva/js/main.js
+++ viva/js/main.js
@@ -3,6 +3,21 @@ $(function(){
     $("#includedFooter").load("blocks/Footer.html"); 
     $('html, body').animate({ scrollTop: 0 })
     FormClose()
+    var $videoContainer = $('#video'),
+		$videoControls = $('.video-control'),
+		$video = $('#myVideo')[0];
+
+	$videoControls.click(function () {
+		if ($video.paused) {
+			$video.play();
+			$videoContainer.addClass('video-is-playing');
+		} else {
+			$video.pause();
+			$videoContainer.removeClass('video-is-playing');
+			//	возвращаем постер
+			$video.load();
+		}
+	});
 })
 function FormClose(){
     $(".redact-2").hide()
--- viva/js/slider.js
+++ viva/js/slider.js
@@ -7,4 +7,51 @@ $(function(){
         autoplaySpeed: 3000,
         dots: true
     })
+    $(".mark__slider").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        responsive: [
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+              },
+            {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+        ]
+    })
+    $(".rerits__slider").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        responsive: [
+            {
+              breakpoint: 650,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+        ]
+    })
 })
