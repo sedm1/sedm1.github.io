9a89242b04038d251435bb0522982088e228f1aa
Николай <novikovn383@gmail.com>
2024-01-30 21:34:53 +0300

    metal

---
+++ metal/js/main.js
@@ -0,0 +1,54 @@
+$(() => {
+    $('.zak__block').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrow: true,
+        dots: false,
+        responsive: [
+            {
+              breakpoint: 1000,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+          ]
+    })
+    $(".met__slider").slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrow: true,
+        dots: false,
+        centerMode: true,
+        centerPadding: '60px',
+        responsive: [
+            {
+              breakpoint: 1000,
+              settings: {
+                centerMode: false,
+                centerPadding: '0px',
+                infinite: true
+              }
+            },
+          ]
+    })
+    $('.main__button, .header__info-button').click(() => {
+      $(".modal__bg").addClass('modal__bg-active')
+      setTimeout(() => {
+        $('.modal__window').addClass('modal__window-active')
+      }, 100)
+    })
+    $('.modal__close').click(() => {
+      $('.modal__window').removeClass('modal__window-active')
+      setTimeout(() => {
+        $(".modal__bg").removeClass('modal__bg-active')
+      }, 200)
+    })
+})
