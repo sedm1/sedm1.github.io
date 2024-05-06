7567a9f440212f7512e10b155ba7d61d1529bcae
Николай <novikovn383@gmail.com>
2024-05-07 00:07:49 +0300

    sil

--- sil/js/main.js
+++ sil/js/main.js
@@ -21,12 +21,58 @@ $('.works__block').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 800,
+          settings: {
+            centerMode: true,
+            slidesToShow: 3,
+            centerPadding: '20px',
+          }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+              centerMode: true,
+              slidesToShow: 2,
+              centerPadding: '20px',
+            }
+          },
+          {
+            breakpoint: 400,
+            settings: {
+              centerMode: true,
+              slidesToShow: 1,
+              centerPadding: '20px',
+            }
+          },
+      ]
 })
 $('.video__block').slick({
     dots: true,
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 2,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+      ]
 })
 $('.header__info-button, .main__item-button, .services__item-button, .about__info-button, .rem__info-button').click(() => {
     $('.modal__bg').css({'display': 'flex'})
@@ -48,4 +94,33 @@ $('.modal__window-close').click(() => {
             $('.modal__bg').css({'display': ''})
         }, 200)
     }, 150)
+})
+
+let width = $(window).width()
+if (width <= 700){
+    $('.services__block').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        centerMode: true,
+        centerPadding: '60px',
+        responsive: [
+            {
+              breakpoint: 500,
+              settings: {
+                centerPadding: '30px',
+              }
+            },
+          ]
+    })
+}
+if(width <= 1330){
+    $('.header__menu-item').click(() => {
+        $('.header__menu-block').removeClass('header__menu-block-active')
+    })
+}
+$('.header__burger').click(() => {
+    $('.header__menu-block').toggleClass('header__menu-block-active')
 })
