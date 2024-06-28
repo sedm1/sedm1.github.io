e65491d3087c495000e3f29b362748550ae5a643
Николай <novikovn383@gmail.com>
2024-06-28 21:12:37 +0300

    ozen

--- ozen/js/main.js
+++ ozen/js/main.js
@@ -1,1 +1,29 @@
-AOS.init();
+$(()=>{
+    AOS.init();
+})
+$('.header__mobile-button, .main__menu-button').click(() => {
+    $('.main__menu').toggleClass('main__menu-active')
+})
+$('.diller__img img').click(() => {
+    $('.modal__bg').css({'display': 'flex'})
+    setTimeout(() => {
+        $('.modal__bg').addClass('modal__bg-active')
+        $('.modal__sert').css({'display': 'block'})
+        setTimeout(() => {
+            $('.modal__sert').addClass('modal__sert-active')
+        }, 150)
+    }, 200)
+})
+$('.modal__bg').click(() => {
+    if ($('.modal__sert').hasClass('modal__sert-active')){
+        $('.modal__sert').removeClass('modal__sert-active')
+        setTimeout(() => {
+            $('.modal__sert').css({'display': ''})
+            $('.modal__bg').removeClass('modal__bg-active')
+            
+            setTimeout(() => {
+                $('.modal__bg').css({'display': ''})
+            }, 150)
+        }, 200)
+    }
+})
