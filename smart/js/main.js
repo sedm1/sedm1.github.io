041079ee646d3e1e2c62f7bdce7b3dac0ef01c68
Николай <novikovn383@gmail.com>
2024-07-02 21:21:49 +0300

    smart

--- smart/js/main.js
+++ smart/js/main.js
@@ -1,9 +1,11 @@
-$('.main__img-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: false,
-    arrows: true
+$(function(){
+    $('.main__img-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true
+    })
 })
 $('.header__info-button').click(() => {
     $('.header__menu-block').addClass('header__menu-block-active')
