c6ca4d30084a949ba63bd36730a4e42b1a80b0c5
Николай <novikovn383@gmail.com>
2024-09-11 23:01:11 +0300

    manik

--- manik/js/main.js
+++ manik/js/main.js
@@ -1,3 +1,11 @@
+let width = $(window).width()
+
+let WhiteArrow = 'img/arrow-link-white.svg'
+let BlackArrow = 'img/arrow-link.svg'
+
+let MenuIcon = 'img/burger.svg'
+let CloseIcon = 'img/close.svg'
+
 $(() => {
     $('.about__slider').slick({
         infinite: true,
@@ -14,6 +22,20 @@ $(() => {
         slidesToScroll: 1,
         dots: false,
         arrows: true,
+        responsive: [
+            {
+              breakpoint: 750,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                  slidesToShow: 1,
+                }
+              }
+          ]
     })
     $('.reviews__slider').slick({
         infinite: true,
@@ -21,7 +43,29 @@ $(() => {
         slidesToScroll: 1,
         dots: false,
         arrows: true,
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+        ]
     })
+    $('.header__menu-item-child').hover(
+      function() {
+        $(this).children('.header__menu-submenu').css({"display": 'flex'})
+      },
+      function(){
+        timerId = setTimeout(() => $(this).children('.header__menu-submenu').css({"display": ''}), 1000);
+      }
+    )
+    $('.header__menu-submenu').hover(
+      function(){
+        clearTimeout(timerId);
+        $(this).css({"display": 'flex'})
+      }
+    )
 })
 
 $('.services__tabs-item').click((e) => {
@@ -35,4 +79,32 @@ $('.services__tabs-item').click((e) => {
     $('.services__block').removeClass('services__block-active')
     $('#services__block-' + id).addClass('services__block-active')
     // alert(id)
+})
+
+$('.link-button').hover(
+  function(){
+    if ($(this).children('img').attr('src') == WhiteArrow){
+      $(this).children('img').attr('src', BlackArrow)
+    } else {
+      $(this).children('img').attr('src', WhiteArrow)
+    }
+  },
+  function(){
+    if ($(this).children('img').attr('src') == WhiteArrow){
+      $(this).children('img').attr('src', BlackArrow)
+    } else {
+      $(this).children('img').attr('src', WhiteArrow)
+    }
+  }
+)
+$('.header__burger').click((e) => {
+  let item = $(e.currentTarget)
+  if (item.children('img').attr('src') == MenuIcon){
+    item.children('img').attr('src', CloseIcon)
+    $('.header__menu').addClass('header__menu-active')
+  } else {
+    item.children('img').attr('src', MenuIcon)
+    $('.header__menu').removeClass('header__menu-active')
+  }
+  
 })
