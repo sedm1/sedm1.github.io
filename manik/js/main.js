46990af5c49fce17d25e891bd3a2207b79db0e2b
Николай <novikovn383@gmail.com>
2024-09-11 19:05:12 +0300

    manik

--- manik/js/main.js
+++ manik/js/main.js
@@ -8,6 +8,20 @@ $(() => {
         fade: true,
         cssEase: 'linear'
     })
+    $('.works__slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    })
+    $('.reviews__slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    })
 })
 
 $('.services__tabs-item').click((e) => {
@@ -17,5 +31,8 @@ $('.services__tabs-item').click((e) => {
     item.addClass('services__tabs-item-active')
 
     let id = item.data('services')
+
+    $('.services__block').removeClass('services__block-active')
+    $('#services__block-' + id).addClass('services__block-active')
     // alert(id)
 })
